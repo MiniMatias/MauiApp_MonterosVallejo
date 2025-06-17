8afb5651abf77834c939581b289cd57a46c09e15
laboratorio <laboratorio@UPOAULA10723>
2025-06-17 07:28:56 -0500

    Agregar archivos de proyecto.

---
+++ MauiApp_MonterosVallejo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp_MonterosVallejo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
